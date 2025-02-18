e37bcc6b76f5266e0ea9bf18e45c724eaf1810f6
Reian Viloria <54141785+naierii@users.noreply.github.com>
2025-02-18 17:08:17 +0800

    fix: camelcase to snake case

--- init.sql
+++ init.sql
@@ -10,21 +10,21 @@ CREATE INDEX idx_username ON Users (username);
 
 CREATE TABLE IF NOT EXISTS Loans (
     id SERIAL PRIMARY KEY,
-    userId INT NOT NULL,
-    loanAmount INT NOT NULL,
-    interestRate DECIMAL(5, 2) NOT NULL,
-    loanTerm INT NOT NULL,
-    startDate DATE NOT NULL,
-    FOREIGN KEY (userId) REFERENCES Users(id)
+    user_id INT NOT NULL,
+    loan_amount INT NOT NULL,
+    interest_rate DECIMAL(5, 2) NOT NULL,
+    loan_term INT NOT NULL,
+    start_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 CREATE TABLE IF NOT EXISTS Payments (
-    paymentId SERIAL PRIMARY KEY,
-    loanId INT NOT NULL,
+    payment_id SERIAL PRIMARY KEY,
+    loan_id INT NOT NULL,
     period INT NOT NULL,
-    paymentDate DATE NOT NULL,
-    paymentAmount DECIMAL(15, 2) NOT NULL,
-    FOREIGN KEY (loanId) REFERENCES Loans(id)
+    payment_date DATE NOT NULL,
+    payment_amount DECIMAL(15, 2) NOT NULL,
+    FOREIGN KEY (loan_id) REFERENCES Loans(id)
 );
 
 INSERT INTO Users (username, email, password) VALUES
@@ -33,11 +33,11 @@ INSERT INTO Users (username, email, password) VALUES
 INSERT INTO Users (id, username, email, password) VALUES
 (2, 'reianviloria', 'reianviloria@gmail.com', 'FEATURE NOT YET AVAILABLE');
 
-INSERT INTO Loans (id, userId, loanAmount, interestRate, loanTerm, startDate) VALUES
+INSERT INTO Loans (id, user_id, loan_amount, interest_rate, loan_term, start_date) VALUES
 (1, 2, 100000, 0.05, 30, '2024-12-01');
 
-INSERT INTO Payments (loanId, period, paymentDate, paymentAmount) VALUES
+INSERT INTO Payments (loan_id, period, payment_date, payment_amount) VALUES
 (1, 1, '2025-01-01', 536.82);
 
-INSERT INTO Payments (loanId, period, paymentDate, paymentAmount) VALUES
+INSERT INTO Payments (loan_id, period, payment_date, payment_amount) VALUES
 (1, 2, '2025-02-01', 536.82);
